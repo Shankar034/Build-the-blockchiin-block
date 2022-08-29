5b37a720c378d311c0a3cd3dd374f334b694f42a
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-08-29 12:52:58 +0545

    Add files via upload

---
+++ block.test.js
@@ -0,0 +1,25 @@
+// const { expect } = require('@babel/types');
+// const { it } = require('node:test');
+// const { describe } = require('yargs');
+const Block = require('./block');
+
+describe('Block', () => {
+
+    let data, lastBlock, block;
+
+    beforeEach(()=> {
+         data = 'bar';
+         lastBlock = Block.genesis();
+         block = Block. mineBlock(lastBlock,data);
+    });
+    it('sets the `data` to match the input', ()=> {
+        expect(block.data).toEqual(data);
+    });
+
+    it('sets the `lastHash` to match the hash of the last block', ()=> {
+
+        expect(block.lastHash).toEqual(lastBlock.hash);
+
+    
+    });
+});
